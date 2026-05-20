c51675abb5f3de9e9df8a6525636c044c82cb7cc
Caleb Overmyer <C.p.overmyer@outlook.com>
2026-05-19 22:35:26 -0700

    Milestone 3 bug fixes

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import teamRouter from './teams/teams.router';
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,9 +18,9 @@ app.use(helmet());
 
 app.use('/', [pokemonRouter, teamRouter]);
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Milestone Project API');
-});
+// app.get('/', (req: Request, res: Response) => {
+//     res.send('Milestone Project API');
+// });
 
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
